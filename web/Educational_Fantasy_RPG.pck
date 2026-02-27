1b598e130d3098a5e5a284d789135ff89447db96
prapars-lang <prapars.lang@gmail.com>
2026-02-27 19:30:38 +0700

    Configure GitHub Pages and Git LFS for Godot 4 Web Export

---
+++ web/Educational_Fantasy_RPG.pck
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe81ba84fd07ea8fffffa7ab2a6aa4b2a787ff3546e1fe3faf157c6406f7bc30
+size 400997936
---
+++ web/coi-serviceworker.js
@@ -0,0 +1,38 @@
+/*! coi-serviceworker v0.1.7 - MIT License - https://github.com/gzuidhof/coi-serviceworker */
+if (typeof window === 'undefined') {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+    self.addEventListener("fetch", (event) => {
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+            return;
+        }
+
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                if (response.status === 0) {
+                    return response;
+                }
+
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            })
+        );
+    });
+} else {
+    const script = document.currentScript;
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register(window.location.protocol + "//" + window.location.host + window.location.pathname).then((registration) => {
+            registration.addEventListener("updatefound", () => {
+                location.reload();
+            });
+        });
+    }
+}
